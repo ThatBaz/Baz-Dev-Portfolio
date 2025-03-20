9975641488baa222cc6b98f3a95ea44ee25859de
Baz-Dev <103224691+ThatBaz@users.noreply.github.com>
2025-03-20 20:18:59 +0000

    Bug fixes, removed clients section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,6 @@ import { FaLocationArrow } from "react-icons/fa6";
 import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
-import "./index.css";
 
 const Hero = () => {
   return (
