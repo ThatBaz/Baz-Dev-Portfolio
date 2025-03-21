91eda12e28ebd9a1e28bd3d520573ab1e9061cec
Barry Jellah <Barryjellah@gmail.com>
2025-03-21 03:13:35 +0200

    modified:   components/Hero.tsx modified:   data/index.ts

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -43,7 +43,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 mb-2">
-          Building ideas into impactful digital solutions
+            Building ideas into impactful digital solutions
           </p>
 
           {/**
@@ -51,11 +51,13 @@ const Hero = () => {
            *
            *  change md:text-6xl, add more responsive code
            */}
-           
+
           <img
-          src="/assets/logo.png"
-          alt="Barry-Dev Logo"
-          className= "" width={300} height={150}
+            src="../assets/logo.png"
+            alt="Barry-Dev Logo"
+            className=""
+            width={300}
+            height={150}
           />
 
           <TextGenerateEffect
