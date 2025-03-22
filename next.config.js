88ee3e8b41359ce26d34bfaa4becc4dd9e15b349
Barry Jellah <Barryjellah@gmail.com>
2025-03-22 03:58:10 +0200

    config mods

--- next.config.js
+++ next.config.js
@@ -1,24 +1,4 @@
 // filepath: next.config.js
-module.exports = {
-    webpack: (config, { isServer }) => {
-      config.module.rules.push({
-        test: /\.glb$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[hash].[ext]',
-              outputPath: 'static/assets/3d/',
-              publicPath: '/_next/static/assets/3d/',
-            },
-          },
-        ],
-      });
-  
-      return config;
-    },
-  };
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
--- next.config.mjs
+++ next.config.mjs
@@ -14,24 +14,6 @@ const nextConfig = {
     unoptimized: true,
     loader: "akamai",
   },
-  
-  webpack: (config, { isServer }) => {
-    config.module.rules.push({
-      test: /\.glb$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[hash].[ext]',
-            outputPath: 'static/assets/3d/',
-            publicPath: '/_next/static/assets/3d/',
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
 };
 
 export default withSentryConfig(nextConfig, {
