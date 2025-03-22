943a2aa2403c0c2b205bbb7572ab25c9f410a431
Barry Jellah <Barryjellah@gmail.com>
2025-03-22 04:35:24 +0200

    Fixed image loading

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -66,7 +66,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "b.jellah@lybotics.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
